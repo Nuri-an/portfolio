038a14683cfa19639d84d124f8136346d19d16e4
Nurian Coelho <nuriancoelho@hotmail.com>
2022-05-25 16:54:10 -0300

    resultado

--- components/ProgressCircle/index.js
+++ components/ProgressCircle/index.js
@@ -1,1 +1,146 @@
+class ProgressCircle extends HTMLElement {
+  constructor() {
+    super();
 
+    this.build();
+  }
+
+  build() {
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    shadowRoot.appendChild(this.styles());
+    shadowRoot.appendChild(this.setStyle());
+
+    const container = this.createContainer();
+    const boxCircular = this.createBoxCircular();
+    const progress = this.createRightProgress();
+    const fillLeftProgress = this.createLeftFillProgress();
+    const hiddenLeftProgress = this.createHiddenLeftProgress();
+    const containerValue = this.createContainerValue();
+    const descriptionSkill = this.createDescriptionSkill();
+
+    boxCircular.appendChild(progress);
+    boxCircular.appendChild(fillLeftProgress);
+    boxCircular.appendChild(hiddenLeftProgress);
+    boxCircular.appendChild(containerValue);
+
+    container.appendChild(boxCircular);
+    container.appendChild(descriptionSkill);
+
+    shadowRoot.appendChild(container);
+  }
+
+  createContainer() {
+    const circular = document.createElement("div");
+    circular.classList.add("circular");
+
+    return circular;
+  }
+
+  createBoxCircular() {
+    const boxCircular = document.createElement("div");
+    boxCircular.classList.add("boxCircular");
+
+    return boxCircular;
+  }
+
+  createRightProgress() {
+    const progressRight = document.createElement("div");
+    progressRight.classList.add("progress-circle");
+    progressRight.classList.add("progress-right");
+
+    return progressRight;
+  }
+
+  createLeftFillProgress() {
+    const progressLeft = document.createElement("div");
+    progressLeft.classList.add("progress-circle");
+    progressLeft.classList.add("progress-left");
+
+    return progressLeft;
+  }
+
+  createHiddenLeftProgress() {
+    const progressHiddenLeft = document.createElement("div");
+    progressHiddenLeft.classList.add("hidden-progress");
+
+    return progressHiddenLeft;
+  }
+
+  createContainerValue() {
+    const cantainerValue = document.createElement("div");
+    cantainerValue.classList.add("container-value");
+    cantainerValue.innerHTML = this.getAttribute("data-value");
+
+    return cantainerValue;
+  }
+
+  createDescriptionSkill() {
+    const descriptionSkill = document.createElement("p");
+    descriptionSkill.classList.add("description-skill");
+    descriptionSkill.innerText = this.getAttribute("data-description");
+
+    return descriptionSkill;
+  }
+
+  setStyle() {
+    // cria a tag style
+    const styles = document.createElement("style");
+
+    const dataValue = this.getAttribute("data-value");
+    const value = (360 / 100) * ((dataValue * 100) / 5) - 180;
+
+    // representa que será um conteúdo de texto
+    styles.textContent = `
+      .progress-right {
+        z-index: 3;
+        clip: rect(140px, ${value > 2 ? "280px" : "140px"}, 280px, 0px);
+        transform: rotate(${value}deg);
+        animation: fill ease-in-out 3s;
+      }
+      
+      .progress-left {
+        z-index: 1;
+        clip: rect(0px, 280px, 140px, 0px);
+        transform: ${value > 2 ? `rotate(${value}deg)` : "initial"};
+        animation: ${value > 2 ? "fillLeft ease-in-out 3s" : "auto"};
+      }
+      
+      @keyframes fill{
+        0% {
+          transform: rotate(${value > 2 ? "-108deg" : "-198deg"});
+        }
+        100% {
+          transform: rotate(${value}deg);
+        }
+      }
+      
+      
+      @keyframes fillLeft{
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(${value}deg);
+        }
+      }
+    `;
+
+    return styles;
+  }
+
+  styles() {
+    var cssId = "progressCircle";
+
+    var link = document.createElement("link");
+    link.id = cssId;
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "components/ProgressCircle/styles.css";
+    link.media = "all";
+
+    return link;
+  }
+}
+
+customElements.define("progress-circle", ProgressCircle);
--- components/ProgressLine/index.js
+++ components/ProgressLine/index.js
@@ -8,21 +8,94 @@ class ProgressLine extends HTMLElement {
   build() {
     const shadowRoot = this.attachShadow({ mode: "open" });
 
+    // insere a tag link com estilos
     shadowRoot.appendChild(this.styles());
+    // insere a tag style com animações customizáveis
     shadowRoot.appendChild(this.setStyle());
 
     const progressLineBox = this.createBox();
+    const description = this.createDescription();
+    const progressLine = this.createProgressLine();
+
+    progressLineBox.appendChild(description);
+    progressLineBox.appendChild(progressLine);
+
     shadowRoot.appendChild(progressLineBox);
   }
 
   createBox() {
+    // cria uma tag div
     const progressLineBox = document.createElement("div");
+    // adiciona nessa tag uma class, para o estilo
+    progressLineBox.classList.add("progress-line-box");
 
+    //retorna a tag criada
     return progressLineBox;
   }
 
+  createDescription() {
+    // cria uma tag div
+    const description = document.createElement("div");
+    // adiciona nessa tag uma class, para o estilo
+    description.classList.add("progress-description");
+
+    // 1-Iniciante; 2-Básico; 3-Intermadiário; 4-Proeficiente; 5-Fluente
+    const dataValue = this.getAttribute("data-value");
+    console.log(dataValue);
+    const valueLanguage =
+      dataValue == 1
+        ? "Iniciante"
+        : dataValue == 2
+        ? "Básico"
+        : dataValue == 3
+        ? "Intermadiário"
+        : dataValue == 4
+        ? "Proeficiente"
+        : dataValue == 5
+        ? "Fluente"
+        : "";
+
+    description.innerHTML = `<p class="language"> ${this.getAttribute(
+      "data-language"
+    )} </p><p class="language-value"> ${valueLanguage} </p>`;
+
+    //retorna a tag criada
+    return description;
+  }
+
+  createProgressLine() {
+    // cria uma tag div
+    const progressLine = document.createElement("div");
+    // adiciona nessa tag uma class, para o estilo
+    progressLine.classList.add("progress-line");
+    progressLine.classList.add("animate-progress");
+
+    return progressLine;
+  }
+
   setStyle() {
+    // cria a tag style
     const styles = document.createElement("style");
+
+    const dataValue = this.getAttribute("data-value");
+    const value = (448.42 / 100) * ((dataValue * 100) / 5);
+
+    // representa que será um conteúdo de texto
+    styles.textContent = `
+      .animate-progress {
+        width: ${value}px;
+        animation: scale ease-in-out 3s;
+      }
+
+      @keyframes scale {
+        0% {
+          width: 89.684px;
+        }
+        100% {
+          width: ${value}px;
+        }
+      }
+    `;
 
     return styles;
   }
